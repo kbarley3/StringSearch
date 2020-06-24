d54c78eeb07b0c2326cadb193b157c6bf6cefe55
kbarley3 <kbarley3@ycp.edu>
2020-06-24 19:39:48 -0400

    Changed Hashmap value to list that stores any found location of small string in big string.

--- stringSearch.java
+++ stringSearch.java
@@ -1,30 +1,29 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class stringSearch {
 
-    public static HashMap<String, String> findStrings(String longStr, String[] arrStr) {
-        HashMap<String, String> stringLocations = new HashMap<String, String>();
+    public static HashMap<String, ArrayList<String>> findStrings(String longStr, String[] arrStr) {
+        HashMap<String, ArrayList<String>> stringLocations = new HashMap<String, ArrayList<String>>();
         String lowerCaseLongStr = longStr.toLowerCase();
 
         for (String str : arrStr) {
+            ArrayList<String> locations = new ArrayList<String>();
             str = str.toLowerCase();
-            stringLocations.put(str, "Not found");
+            stringLocations.put(str, locations);
 
             for (int i = 0; i < lowerCaseLongStr.length(); i++) {
-                // Found the start of the string
+                // Found the first character of the small string
                 if (lowerCaseLongStr.charAt(i) == str.charAt(0)) {
                     // Check the following characters to see if it is a match.
                     for (int j = 1; j < str.length(); j++) {
                         if ((i + j) >= lowerCaseLongStr.length() || lowerCaseLongStr.charAt(i + j) != str.charAt(j)) {
                             break;
-                        } else {
+                        }
+                        else {
+                            // if last character in small string is matched, then add found location.
                             if (j == str.length() - 1) {
-                                if (!stringLocations.get(str).equals("Not found")) {
-                                    // Appends additional locations to the key's value.
-                                    stringLocations.replace(str, stringLocations.get(str) + "; Indices " + i + " to " + (i + j));
-                                } else {
-                                    stringLocations.replace(str, "Indices " + i + " to " + (i + j));
-                                }
+                                stringLocations.get(str).add("Indices " + i + " to " + (i + j));
                             }
                         }
                     }
