dc223ed5425bec13aedfbc9a55fd5d8b507a5902
kbarley3 <kbarley3@ycp.edu>
2020-06-24 19:10:52 -0400

    now adds indices if found more than once

--- stringSearch.java
+++ stringSearch.java
@@ -2,54 +2,45 @@ import java.util.HashMap;
 
 public class stringSearch {
 
-    public static HashMap<String, String> findStrings(String longStr, String[] arrStr)
-    {
-        HashMap<String, String> location = new HashMap<String, String>();
+    public static HashMap<String, String> findStrings(String longStr, String[] arrStr) {
+        HashMap<String, String> stringLocations = new HashMap<String, String>();
         String lowerCaseLongStr = longStr.toLowerCase();
 
-        for (String str : arrStr)
-        {
-            location.put(str, "Not found");
+        for (String str : arrStr) {
             str = str.toLowerCase();
+            stringLocations.put(str, "Not found");
 
-            for (int i = 0; i < lowerCaseLongStr.length(); i++)
-            {
+            for (int i = 0; i < lowerCaseLongStr.length(); i++) {
                 // Found the start of the string
-                if (lowerCaseLongStr.charAt(i) == str.charAt(0))
-                {
+                if (lowerCaseLongStr.charAt(i) == str.charAt(0)) {
                     // Check the following characters to see if it is a match.
-                    for (int j = 1; j < str.length(); j++)
-                    {
-                        if ((i + j) < lowerCaseLongStr.length() && lowerCaseLongStr.charAt(i + j) != str.charAt(j))
-                        {
+                    for (int j = 1; j < str.length(); j++) {
+                        if ((i + j) >= lowerCaseLongStr.length() || lowerCaseLongStr.charAt(i + j) != str.charAt(j)) {
                             break;
-                        }
-                        else
-                        {
-                            if (j == str.length() - 1)
-                            {
-                                location.replace(str, "Indices " + i + " to " + (i + j));
+                        } else {
+                            if (j == str.length() - 1) {
+                                if (!stringLocations.get(str).equals("Not found")) {
+                                    // Appends additional locations to the key's value.
+                                    stringLocations.replace(str, stringLocations.get(str) + "; Indices " + i + " to " + (i + j));
+                                } else {
+                                    stringLocations.replace(str, "Indices " + i + " to " + (i + j));
+                                }
                             }
                         }
                     }
                 }
             }
         }
-
-        return location;
+        return stringLocations;
     }
 
-    public static void main(String[] args)
-    {
-
+    public static void main(String[] args) {
         // Given string
         String str = "Time to search for some strings!";
         // Array of smaller strings
-        String[] arrStr = new String[] {"hello", "ear", "for", "rings", "arch", "time"};
-
+        String[] arrStr = new String[]{"hello", "ear", "For", "Rings", "Arch", "time", "me", "gs!", "gs!h"};
         // Locate smaller strings and display locations
         System.out.println(findStrings(str, arrStr));
-
     }
 
 }
